d41c03e0476be2f8b72c9383367609b7c6a1dbad
Steven Blakely <email@autisticgeek.com>
2023-03-06 23:22:49 -0700

    Working prototype

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  Stack,
+  TextField,
+} from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import React, { useRef, useState } from "react";
+import * as jose from "jose";
+import { Buffer } from "buffer";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
+  const [verified, setVerified] = useState(false);
+  const [encoded, setEncoded] = useState("");
+  const [message, setMessage] = useState("");
+  const [key, setKey] = useState("");
+
+  const _decoder = async () => {
+    const secret = new TextEncoder().encode(key);
+    await jose
+      .jwtVerify(encoded, secret, {
+        algorithms: ["HS256"],
+      })
+      .then((payload) => {
+        setMessage(payload?.payload?.message);
+        setVerified(true);
+      })
+      .catch((err) => {
+        const bufferObj = Buffer.from(encoded.split(".")[1], "base64");
+        setMessage(JSON.parse(bufferObj.toString("utf8")).message);
+        setVerified(false);
+      });
+  };
+
+  const signJwt = (payload) => {
+    const secret = new TextEncoder().encode(key);
+    return new jose.SignJWT(payload)
+      .setProtectedHeader({ alg: "HS256" })
+      .setIssuedAt()
+      .sign(secret);
+  };
+
+  const _encoder = async () => {
+    setEncoded(await signJwt({ message }).catch(() => "Missing Key"));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container><Stack height="100vh" justifyContent="center" flex={1}>
+        <Box>
+          <TextField
+            fullWidth
+            label="Encoded"
+            multiline
+            value={encoded}
+            onChange={(e) => setEncoded(e.target.value)}
+          />
+        </Box>
+        <Stack direction="row" spacing={2} sx={{ mt: 2 }}>
+          <Button variant="contained" onClick={() => _decoder()}>
+            Decode
+          </Button>
+          <Button variant="contained" onClick={() => _encoder()}>
+            Encode
+          </Button>
+          <TextField
+            fullWidth
+            multiline
+            label="Common Key"
+            value={key}
+            onChange={(e) => setKey(e.target.value)}
+          />
+          <Button variant="contained" color={verified ? "success" : "error"}>
+            {verified ? "Valid" : "Invalid"}
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() => {
+              setEncoded("");
+              setMessage("");
+              setVerified(false);
+            }}
+          >
+            Clear
+          </Button>
+        </Stack>
+        <Box sx={{ pt: 2 }}>
+          <TextField
+            fullWidth
+            multiline
+            label="Decoded"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </Box></Stack>
+      </Container>
+    </ThemeProvider>
   );
 }
 
